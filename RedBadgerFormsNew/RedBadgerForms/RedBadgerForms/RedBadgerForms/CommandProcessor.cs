1663383b853774e11e1ad0777b79624352648996
Jamie Foster <55837757+JamieF15@users.noreply.github.com>
2024-11-21 15:37:32 +0000

    Can now move in 2 directions.

---
+++ RedBadgerFormsNew/RedBadgerForms/RedBadgerForms/RedBadgerForms/CommandProcessor.cs
@@ -0,0 +1,39 @@
+﻿namespace RedBadgerForms
+{
+    internal class CommandProcessor
+    {
+        public static void ProcessCommand(string command, Robot robot)
+        {
+            if (robot.GetIsLost())
+            {
+                return;
+            }
+            for (int i = 0; i < command.Length; i++)
+            {
+                switch(command[i].ToString().ToUpper()) {
+                    case "N":
+                        robot.SetOrientation(command[i]);
+                        continue;
+                    case "E":
+                        robot.SetOrientation(command[i]);
+                        continue;
+                    case "S":
+                        robot.SetOrientation(command[i]);
+                        continue;
+                    case "W":
+                        robot.SetOrientation(command[i]);
+                        continue;
+                    case "L":
+                        robot.MoveLeft();
+                        continue;
+                    case "R":
+                        robot.MoveRight();
+                        continue;
+                    case "F":
+                        robot.MoveForward();
+                        continue;
+                }
+            }
+        }
+    }
+}
